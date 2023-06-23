4f145c52b01df800b5ff2157407ada313d907017
Radu Lefter <lefter_radu@yahoo.com>
2023-06-23 15:45:07 +0100

    created func to reset selected piece

--- script.js
+++ script.js
@@ -84,4 +84,19 @@ function resetBorders() {
     getSelectedPiece();
 }
 
+// resets selected piece properties
+function resetSelectedPieceProperties() {
+    selectedPiece.pieceId = -1;
+    selectedPiece.pieceId = -1;
+    selectedPiece.isKing = false;
+    selectedPiece.seventhSpace = false;
+    selectedPiece.ninthSpace = false;
+    selectedPiece.fourteenthSpace = false;
+    selectedPiece.eighteenthSpace = false;
+    selectedPiece.minusSeventhSpace = false;
+    selectedPiece.minusNinthSpace = false;
+    selectedPiece.minusFourteenthSpace = false;
+    selectedPiece.minusEighteenthSpace = false;
+}
+
 givePiecesEventListeners();
